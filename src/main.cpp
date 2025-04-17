7c95bce560217d4e9194f6a1c626220ff23ce39c
Alain <alain.sn.66@gmail.com>
2025-04-17 17:05:40 +0200

    signal

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include <string>
+#include <csignal>
 #include "Server.hpp"
 
-#include "Server.hpp"
+Server* g_server = NULL;
+
+void signalHandler(int signum)
+{
+	if (signum == SIGINT && g_server != NULL)
+	{
+		g_server->stop();
+		std::cout << "Stopping server ..." << std::endl;
+	}
+}
 
 int main(int argc, char **argv, char **envp)
 {
 	(void)envp;
-	std::string config_file_name = (argc == 2) ? argv[1] : "default";
-	std::cout << config_file_name << std::endl;
+	std::string config_file_name = (argc == 2) ? argv[1] : "";
 
-    try
-    {
-        Server server("localhost", "8080");
-        server.listen();
-        std::cout << "Server is now listening for connections..." << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        std::cerr << "Error: " << e.what() << std::endl;
-    }
+	try
+	{
+		Server server("localhost", "8080");
+		g_server = &server;
+		signal(SIGINT, signalHandler);
+		server.listen();
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Error: " << e.what() << std::endl;
+	}
 
-    return 0;
+	return 0;
 }
