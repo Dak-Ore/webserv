a3f1086f060fc8ffd54aa72609e8a2dbb4076084
Alain <alain.sn.66@gmail.com>
2025-04-22 11:09:30 +0200

    cookies

--- includes/HttpMessage.hpp
+++ includes/HttpMessage.hpp
@@ -7,13 +7,15 @@ class HttpMessage
 {
 protected:
 	std::map<std::string, std::string>	_headers;
+	std::map<std::string, std::string>	_cookies;
 	std::string							_body;
 	std::string							_version;
 public:
 	const std::map<std::string, std::string>	&getHeaders() const;
 	const std::string &getHeader(std::string key) const;
+	const std::map<std::string, std::string>	&getCookies() const;
+	const std::string &getCookie(std::string key) const;
 	const std::string							&getBody() const;
 	const std::string							&getVersion() const;
-	virtual std::string					toString() = 0;
 };
 
--- src/HttpMessage.cpp
+++ src/HttpMessage.cpp
@@ -2,5 +2,7 @@
 
 const std::map<std::string, std::string>	&HttpMessage::getHeaders() const {return (this->_headers);}
 const std::string &HttpMessage::getHeader(std::string key) const {return (this->_headers.at(key));}
+const std::map<std::string, std::string>	&HttpMessage::getCookies() const {return (this->_cookies);}
+const std::string &HttpMessage::getCookie(std::string key) const {return (this->_cookies.at(key));}
 const std::string	&HttpMessage::getBody() const {return (this->_body);}
 const std::string	&HttpMessage::getVersion() const {return (this->_version);}
