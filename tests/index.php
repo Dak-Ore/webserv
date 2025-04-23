c1eab252465541e01921d1bf62d232429c247e31
mcolonna <mcolonna@student.42perpignan.fr>
2025-04-23 18:28:11 +0200

    dev: add tests

--- tests/index.php
+++ tests/index.php
@@ -1,12 +1,22 @@
 <pre><?php
 echo "hi i am a php file\n";
+echo "\n";
 
-if (isset($_GET["name"])) {
+if (isset($_GET["name"]))
 	echo "your name is " . $_GET["name"] . "\n";
-} else {
+else
 	echo "define a name with ?name=<your-name>\n";
-}
+echo "\n";
+
+if (isset($_POST["favoritemeal"]))
+	echo "your favorite meal is " . $_POST["favoritemeal"] . " :)\n";
+else
+	echo "you can also try to POST the 'favoritemeal' field :)\n";
+echo "\n";
+
 echo "this script is " . $_SERVER['SCRIPT_NAME'] . "\n";
+echo "\n";
+
 print_r($_SERVER);
 
 ?></pre>
