5f0d5568daa989e9bb5152736de29636ce2a4945
Dak - rsebasti <rsebasti@student.42perpignan.fr>
2025-05-13 13:12:03 +0200

    Add comment

--- includes/Config.hpp
+++ includes/Config.hpp
@@ -9,31 +9,85 @@
 # include <cmath>
 # include <cstdlib>
 
+/*
+    Value that can be in LocationConfig and ServerConfig
+*/
 class Config
 {
 protected:
+    /*
+        Link to the directory that contain the pages.
+        A server or a location can have different root, default is www
+    */
     std::string _root;
-    std::vector<std::string> _index;
+    /*
+    List of index, default index is index.html
+    */
+   std::vector<std::string> _index;
+    /*
+       True : 
+       
+       False :
+           Check if at least an index is valid 
+   */
+   bool _autoIndex;
+   /*
+        Size of request body, if not specified there is no limit (or maybe linux limits)
+        Value is digits and can be followed by k/m/g (not case sensitive)
+    */
     size_t _clientMaxBodySize;
+    /*
+        Key   : int error ex 404
+        Value : string path to error page
+    */
     std::map<int, std::string> _errorPages;
+    /*
+        List of cgi allowed in the server / location.
+    */
     std::vector<std::string> _cgiExtension;
+    /*
+        Default : false
+    */
     bool _uploadEnabled;
+    /*
+        Where we upload the file, need to check if the path is correct and is a directory
+    */
     std::vector<std::string> _uploadPath;
+    /*
+        Method allowed in the server / location, all allowed.
+        We handle only GET / POST / DELETE.
+    */
     std::vector<std::string> _allowedMethods;
-    bool _autoIndex;
 public:
+    // default constructor
 	Config();
+    // default destructor
 	~Config();
-	std::string getRoot() const;
-    std::vector<std::string> getIndex() const;
-	size_t getClientMaxBodySize() const;
-    const std::map<int, std::string>& getErrorPages() const;
-	bool getAutoIndex() const;
-    std::vector<std::string> getCgiExtension()const;
-    bool getUploadEnabled() const;
-    std::vector<std::string> getUploadPath()const;
-	std::vector<std::string> getAllowedMethods()const;
+    
+    // GETTERS
+	std::string  getRoot() const;
+    std::vector<std::string>            getIndex() const;
+	size_t                              getClientMaxBodySize() const;
+    const std::map<int, std::string>&   getErrorPages() const;
+	bool                                getAutoIndex() const;
+    std::vector<std::string>            getCgiExtension()const;
+    bool                                getUploadEnabled() const;
+    std::vector<std::string>            getUploadPath()const;
+	std::vector<std::string>            getAllowedMethods()const;
+
+    /*
+        Check is parsed var is valid.
+        current check : 
+            - root : check is valid character
+            - allow_methods : Check if element of the list is GET / POST / DELETE
+    */
 	void checkVar();
+    /**
+    	* @brief Handle the parsing of a line for children.
+        * @param key key of a parameter on the config file.
+        * @param value value(s) of a parameter on the config file.
+        * @param line complete line to handle certain case.
+    */
 	int	parseVar(std::string key, std::string value, std::string line);
 	virtual void	findElement(std::string line) = 0;
 };
--- includes/LocationConfig.hpp
+++ includes/LocationConfig.hpp
@@ -10,24 +10,40 @@
 
 class LocationConfig : public Config
 {
-public:
-    // Default Constructor
-    LocationConfig(std::string content);
-    
-	void	findElement(std::string line);
-    void	splitPush(std::string line, int kind);
-	// Destructor
-    virtual ~LocationConfig();
-	void		checkConfig();
-	void		print();
-	std::string getPath() const;
-    bool getHasRedirection() const;
-    std::pair<int, std::string> getRedirection() const;
-	std::string getRelativePath(const std::string& path) const;
 private:
 	std::string _path;
     bool _hasRedirection;
     std::pair<int, std::string> _redirection;
+public:
+    // Default Constructor
+    LocationConfig(std::string content);
+	// Destructor
+    virtual ~LocationConfig();
+    /**
+        * @brief extract key and value of the line and search if it's a valid parameter.
+        * @param line contain a line of content 
+     */
+	void	findElement(std::string line);
+    /*
+        Check is parsed var is valid.
+        current check : 
+            - path check if character is valid
+            - redirection check if int part is correct (>= 100 OR <= 599) and string part contains valid charecter
+    */
+	void		checkConfig();
+
+    /**
+        * TO DO 
+     */
+    std::string getRelativePath(const std::string& path) const;
+    
+    // GETTER
+    std::pair<int, std::string> getRedirection() const;
+    std::string getPath() const;
+    bool getHasRedirection() const;
+
+    // DEBUG
+	void		print();
 };
 
 #endif
